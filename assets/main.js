00d5a61ce8ce1614139f563e71dbb7dc2498a46b
Santiago Madariaga <santiagoamadariaga@gmail.com>
2022-10-24 12:14:33 -0300

    filtros puestos

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,4 @@
-const productsContent = document.querySelector(".container_populares");
-const product = document.getElementById("product");
+const products = document.getElementById("product");
 // - precio total del carrito
 const categoriesContent = document.querySelector(".container_categories");
 const categories = document.querySelectorAll(".card_category");
@@ -38,71 +37,77 @@ const renderProduct = (product) => {
         <div class="img_popular">
             <img src=${img} alt=${name}>
         </div>
-        <div class="text-populares">
-            <p class="text_two">${name}</p>
-            <p class="opacity">${descr}</p>
-            <p class="price_card">$ ${price}</p>
+        <div class="content_text_cards">
+          <div class="text-populares">
+              <p class="text_two">${name}</p>
+              <p class="opacity">${descr}</p>
+              <p class="price_card">$ ${price}</p>
 
-        </div>
-        <div class="btn">
-            <button class="btn_add"
-            data-id="${id}"
-            data-name="${name}"
-            data-descr="${descr}"
-            data-price="${price}"
-            data-img="${img}">Agregar</button>
+          </div>
+          <div class="btn">
+              <button class="btn_add"
+              data-id="${id}"
+              data-name="${name}"
+              data-descr="${descr}"
+              data-price="${price}"
+              data-img="${img}">Agregar</button>
+          </div>
         </div>
     </div> `;
 };
 
-const renderFilteredProducts = (category) => {
-  const productsList = productsData.filter((product) => {
-    product.category === category;
-  });
-  productsContent.innerHTML = productsList.map(renderProduct).join("");
-};
-
-const renderPopularProducts = (popular) => {
-  const productsList = productsData.filter(
-    (product) => product.popular === true
-  );
-  productsContent.innerHTML = productsList.map(renderProduct).join("");
-};
-
-const renderProducts = (popular = undefined, category = undefined) => {
-  if (!category) {
-    renderPopularProducts(popular);
-    return;
-  }
-  renderFilteredProducts(category);
-};
-
-const changeActiveState = (e) => {
-  const categories = [...categoriesContent];
+const renderFilteredProducts = (e) => {
   const selectedCategory = e.target.dataset.category;
 
-  categories.forEach((categoryCard) => {
-    if (categoryCard.dataset.category != selectedCategory) {
-      categoryCard.classList.remove("active");
-      return;
-    }
-    categoryCard.classList.add("active");
+  const productsList = productsData.filter((products) => {
+    return products.category === selectedCategory;
   });
+  products.innerHTML = productsList.map(renderProduct).join("");
 };
 
-const applyFilter = (e) => {
-  //   console.log(e.target.classList.contains("card_category"));
-  if (!e.target.classList.contains("card_category")) return;
-  changeActiveState(e);
-  if (!e.target.dataset.category) {
-    productsContent.innerHTML = "";
-    renderProducts();
-  }
+const renderPopularProducts = () => {
+  const productsList = productsData.filter((products) => {
+    return products.popular === true;
+  });
+  products.innerHTML = productsList.map(renderProduct).join("");
 };
+
+// const renderProducts = (e) => {
+//   // const selectedCategory = e.target.dataset.category;
+
+//   if (e.target.dataset.category === productsData.popular) {
+//     renderPopularProducts();
+//     return;
+//   }
+//   renderFilteredProducts();
+// };
+
+// const changeActiveState = (e) => {
+//   const categories = [...categoriesContent];
+//   const selectedCategory = e.target.dataset.category;
+
+//   categories.forEach((categoryCard) => {
+//     if (categoryCard.dataset.category != selectedCategory) {
+//       categoryCard.classList.remove("active");
+//       return;
+//     }
+//     categoryCard.classList.add("active");
+//   });
+// };
+
+// const applyFilter = (e) => {
+//   console.log(e.target.classList.contains("card_category"));
+//   if (!e.target.classList.contains("card_category")) return;
+//   changeActiveState(e);
+//   if (!e.target.dataset.category) {
+//     productsContent.innerHTML = "";
+//     renderProducts();
+//   }
+// };
 
 const init = () => {
-  renderProducts();
-  categoriesContent.addEventListener("click", applyFilter);
+  renderPopularProducts();
+  categoriesContent.addEventListener("click", renderFilteredProducts);
 };
 
 init();
