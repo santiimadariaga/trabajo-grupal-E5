cc5cc3363b9f968465e07d01f371eaed811165c8
Santiago Madariaga <santiagoamadariaga@gmail.com>
2022-10-27 11:53:02 -0300

    productos añadidos al carrito

--- assets/main.js
+++ assets/main.js
@@ -2,13 +2,15 @@ const products = document.getElementById("product");
 const categoriesContent = document.querySelector(".container_categories");
 const categories = document.querySelectorAll(".card_category");
 // - precio total del carrito
-const buyAdd = document.querySelector(".btn_add");
+const totalPrice = document.getElementById("total");
+const buyBtn = document.querySelector(".buy_btn");
+const productAdd = document.getElementById("btn_add");
 const cartBtn = document.getElementById("icon-cart");
 const cartList = document.getElementById("cartList");
+const contentProducts = document.querySelector(".content_cart_products");
+const closeCart = document.querySelector(".check-out");
+const viewMore = document.getElementById("viewMore");
 const overlay = document.getElementById("overlay");
-// - el contenedor del carrito en sí
-// const cartMenu = document.getElementById("");
-// - overlay
 
 let cart = JSON.parse(localStorage.getItem(`cart`)) || [];
 
@@ -32,7 +34,7 @@ const renderProduct = (product) => {
 
           </div>
           <div class="btn">
-              <button class="btn_add"
+              <button class="btn_add" id="btn_add"
               data-id="${id}"
               data-name="${name}"
               data-descr="${descr}"
@@ -62,10 +64,6 @@ const renderFilteredProducts = (e) => {
   } else {
     renderPopularProducts();
   }
-  // } else if (selectedCategory === "popular") {
-  //   products.innerHTML = "";
-  //   renderPopularProducts();
-  // console.log(selectedCategory === "popular");
 };
 
 const renderPopularProducts = () => {
@@ -103,6 +101,8 @@ const renderPopularProducts = () => {
 //   }
 // };
 
+/* ABRIR Y CERRAR CARRITO */
+
 const toggleCart = () => {
   cartList.classList.remove("none");
   cartList.classList.toggle("open_cart");
@@ -118,11 +118,125 @@ const closeOnOverlayClick = () => {
   overlay.classList.remove("overlay");
 };
 
+const closeOnCloseButton = () => {
+  cartList.classList.add("none");
+  overlay.classList.remove("overlay");
+};
+
+/* AGREGADO DE PRODUCTOS Y MANEJO DEL CARRITO */
+
+const renderCartProduct = (cartProduct) => {
+  const { id, name, img, descr, price, quantity } = cartProduct;
+
+  return `
+  <div class="cart_products">
+    <div class="product_img">
+        <img src=${img} alt=${name} />
+    </div>
+    <div class="product_text">
+      <p class="product_name">${name}</p>
+      <p class="opacity">${descr}</p>
+      <p class="product_price">$ ${price}</p>
+    </div>
+    <div class="product_button">
+      <button class="minus">
+        <p class="pos" data-id=${id}>-</p>
+      </button>
+        <p class="quantity">${quantity}</p>
+      <button>
+        <p class="pos_two" data-id=${id}>+</p>
+      </button>
+    </div>
+  </div>
+  `;
+};
+
+const renderCart = () => {
+  // Si el carrito está vacío
+  if (!cart.length) {
+    contentProducts.innerHTML = `<p class="opacity">No hay nada para mostrar aquí.</p>`;
+    return;
+  }
+  // Renderizamos lo que haya
+  contentProducts.innerHTML = cart.map(renderCartProduct).join("");
+};
+
+const getCartTotal = () => {
+  return cart.reduce(
+    (acc, cur) => acc + Number(cur.price) * Number(cur.quantity),
+    0
+  );
+};
+
+const showTotal = () => {
+  total.textContent = `$ ${Number(getCartTotal()).toFixed(2)}`;
+};
+
+const disabledBtn = (btn) => {
+  if (!cart.length) {
+    btn.classList.add("disabled");
+    return;
+  }
+  btn.classList.remove("disabled");
+};
+
+/* AÑADIDO DE PRODUCTOS */
+
+const createProductData = (id, name, descr, price, img) => {
+  return { id, name, descr, price, img };
+};
+
+const isExistingCartProduct = (product) => {
+  return cart.find((item) => {
+    item.id === product.id;
+  });
+};
+
+const addUnitToProduct = (product) => {
+  cart.map((cartProduct) => {
+    return cartProduct.id === product.id
+      ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+      : cartProduct;
+  });
+};
+
+const createCartProduct = (product) => {
+  cart = [...cart, { ...product, quantity: 1 }];
+};
+
+const addProduct = (e) => {
+  if (!e.target.classList.contains("btn_add")) return;
+
+  console.log(e.target.classList.contains("btn_add"));
+
+  const { id, name, descr, price, img } = e.target.dataset;
+
+  const product = createProductData(id, name, descr, price, img);
+
+  // El producto existe en el carrito
+  if (isExistingCartProduct(product)) {
+    addUnitToProduct(product);
+  } else {
+    // El producto no existe
+    createCartProduct(product);
+  }
+  saveToLocalStorage(cart);
+  renderCart(cart);
+  showTotal(cart);
+  disabledBtn(buyBtn);
+};
+
 const init = () => {
   renderPopularProducts();
   categoriesContent.addEventListener("click", renderFilteredProducts);
   cartBtn.addEventListener("click", toggleCart);
   overlay.addEventListener("click", closeOnOverlayClick);
+  closeCart.addEventListener("click", closeOnCloseButton);
+  viewMore.addEventListener("click", closeOnCloseButton);
+  document.addEventListener("DOMContentLoaded", renderCart);
+  document.addEventListener("DOMContentLoaded", showTotal);
+  products.addEventListener("click", addProduct);
+  disabledBtn(buyBtn);
 };
 
 init();
